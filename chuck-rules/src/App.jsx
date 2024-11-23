dd8f35d6433f9f5602274b2d87148dd4d47478bd
username <178353419+while-maybe@users.noreply.github.com>
2024-11-23 11:52:34 +0000

    refactor: simplify code

--- chuck-rules/src/App.jsx
+++ chuck-rules/src/App.jsx
@@ -7,9 +7,9 @@ function App() {
   const [error, setError] = useState(null);
 
   // Handles input changes
-  const handleChange = (e) => {
-    setMaxLength(e.target.value);
-  };
+  // const handleChange = (e) => {
+  //   setMaxLength(e.target.value);
+  // };
 
   // Handles search click to get status info
   const handleSearch = async () => {
@@ -17,14 +17,13 @@ function App() {
     try {
       const response = await axios.get(
         `https://api.chucknorris.io/jokes/random`
-        // `https://catfact.ninja/facts?limit=${maxLength}`
       );
 
       // destructure the data from the response
-      const data = response.data.value;
+      const data = response.data;
       console.log(data);
 
-      setChuckJoke(data);
+      setChuckJoke(data.value);
     } catch (err) {
       setError("Could not retrieve information. Please check the status code.");
       setStatusInfo(null);
@@ -50,7 +49,6 @@ function App() {
       ) : (
         <h2>no joke to display :(</h2>
       )}
-      {/* TODO: If there are no facts, display "no facts to display" */}
     </div>
   );
 }
